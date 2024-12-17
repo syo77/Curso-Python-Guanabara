022173438ae83d8f472be9e675bc45e0133386d8
syo77 <yurivsv77@gmail.com>
2024-12-17 19:02:28 -0300

    ex068

--- mundo2/ex068.py
+++ mundo2/ex068.py
@@ -1,10 +1,13 @@
 """
 Faça um programa que jogue par ou ímpar com o computador. O jogo só será interrompido quando o jogador PERDER, mostrando o total de vitórias consecutivas que ele conquistou no final do jogo
 """
-
 from random import randint
-computador = randint(0, 10)
 jogador = 0
+resultado = 0
+vitorias = 0
+print("---"*10)
+print("JOGO DO PAR OU ÍMPAR")
+print("---"*10)
 while True:
     while True:
         parouimpar = str(input("Par ou Ímpar? (P/I): ")).strip()[0]
@@ -12,6 +15,7 @@ while True:
             break
         else:
             print("Inválido. Tente novamente!")
+    computador = randint(0, 10)
     while True:
         jogador = int(input("Digite um número: "))
         if jogador > 10 or jogador < 0:
@@ -19,10 +23,27 @@ while True:
         else:
             break
     resultado = jogador + computador
-    par = resultado % 2 == 0
-    impar = resultado % 2 != 0
-    # if resultado == par:
-    # if parouimpar.lower() == 
-    if jogador != computador:
-        break
-
+    if parouimpar.lower() == 'p':
+        if resultado % 2 == 0:
+            vitorias += 1
+            print("-=-"*10)
+            print("VOCÊ GANHOU!")
+            print(f"O computador jogou {computador}")
+        else:
+            break
+    if parouimpar.lower() == 'i':
+        if resultado % 2 != 0:
+            vitorias += 1
+            print("-=-"*10)
+            print("VOCÊ GANHOU!")
+            print(f"O computador jogou {computador}")
+        else:
+            break
+print("-=-"*10)
+print("VOCÊ PERDEU!")
+print(f"Computador jogou {computador}")
+if resultado % 2 == 0:
+    print(f"O total foi {resultado} que é par")
+else:
+    print(f"O total foi {resultado} que é ímpar")
+print(f"Total de vitórias: {vitorias}")
