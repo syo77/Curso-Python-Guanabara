6686802b8fc1346a3dc7486cde5514ab0405fe0b
syo77 <yurivsv77@gmail.com>
2025-01-08 19:49:00 -0300

    ex021

--- mundo1/ex021.py
+++ mundo1/ex021.py
@@ -2,8 +2,10 @@
 Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
 """
 import pygame
+from os import system
 pygame.init()
 pygame.mixer.music.load("mundo1/ex021.mp3")
 pygame.mixer.music.play()
-input()
+system('cls')
+input("Pressione enter para interromper: ")
 pygame.event.wait()
